3647ad7069f4a94e20ce3a483fdbc483cce14537
Dmitry Shemin <shemindmitry@gmail.com>
2025-07-18 23:17:44 +0700

    feat: install apps

--- setup.sh
+++ setup.sh
@@ -5,6 +5,68 @@ set -e
 _CWD=$(dirname "$(readlink -f "$0")")
 
 main() {
+	log "Install apps"
+	install
+	log "Setup rust"
+	setup_rust
+	log "Setup links"
+	links
+}
+
+install() {
+	local os=$(uname -o)
+
+	log "Install on $os"
+	case "$os" in
+	"Darwin")
+		install_darwin
+		;;
+	esac
+}
+
+install_darwin() {
+	# Install brew, because we will install everything through that command.
+	if ! has_cmd "brew"; then
+		log "Install brew"
+		sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	fi
+
+	CMDS[0]="tmux"
+	CMDS[1]="neovim"
+	CMDS[2]="exa"
+	CMDS[3]="bat"
+	CMDS[4]="kubernetes-cli"
+	CMDS[5]="go"
+	CMDS[6]="htop"
+	CMDS[7]="fzf"
+	CMDS[8]="lua"
+	CMDS[9]="git-delta"
+	CMDS[10]="ripgrep"
+	CMDS[11]="navi"
+	CMDS[12]="rustup"
+	CMDS[13]="derailed/k9s/k9s"
+	CMDS[14]="jq"
+	CMDS[15]="font-fira-code-nerd-font"
+
+	for x in ${CMDS[@]}; do
+		brew_install "$x"
+	done
+}
+
+brew_install() {
+	local pkg="$1"
+
+	brew list | grep $pkg > /dev/null && log "$pkg already installed" && return
+
+	log "Install $pkg"
+	brew install $pkg
+}
+
+has_cmd() {
+	[ -x "$(command -v $1)" ] || false
+}
+
+links() {
 	link .navi $HOME/.navi
 	link bat $HOME/.config/bat
 	link nvim $HOME/.config/nvim
@@ -28,6 +90,10 @@ link() {
 
 	ln -s $from $to
 	log "Create symlink from $from to $to"
+}
+
+setup_rust() {
+	rustup update
 }
 
 log() {
